7238660af823c446fdd7b47817ecf6c23495833f
Angie Moya <angiemoyau.1998@gmail.com>
2025-08-15 02:59:31 -0500

    main setup updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Datagrid } from './components/datagrid/datagrid';
+import { NgbdModalDelete } from './components/modaldelete/modaldelete';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Datagrid, NgbdModalDelete],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('phonebook');
+  projectName = 'Phone Book';
 }
