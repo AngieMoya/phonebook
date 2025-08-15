4b57ec810ea782c50b02949339476240b47d7234
Angie Moya <angiemoyau.1998@gmail.com>
2025-08-15 02:53:46 -0500

    dropdown component created

---
+++ src/app/components/dropdown/dropdown.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-dropdown',
+  imports: [NgbDropdownModule],
+  templateUrl: './dropdown.html',
+  styleUrl: './dropdown.css'
+})
+export class Dropdown {
+
+}
