f28fbd637894d370563310262995ee8372f02093
Angie Moya <angiemoyau.1998@gmail.com>
2025-08-15 22:52:24 -0500

    Reactive form apprach in the Modal component

--- src/app/components/modal/modal.ts
+++ src/app/components/modal/modal.ts
@@ -1,15 +1,15 @@
 import {
   Component,
-  EventEmitter,
-  inject,
   Input,
   Output,
-  signal,
+  EventEmitter,
   TemplateRef,
+  inject,
+  signal,
   WritableSignal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Dropdown, DropdownOption } from '../dropdown/dropdown';
 import { CONTACT_TYPES } from '../../constants/contact-types';
 import { CreateContactDto } from '../../services/contacts.service';
@@ -17,13 +17,14 @@ import { CreateContactDto } from '../../services/contacts.service';
 @Component({
   selector: 'ngbd-modal',
   standalone: true,
-  imports: [FormsModule, Dropdown],
+  imports: [ReactiveFormsModule, Dropdown],
   templateUrl: './modal.html',
 })
 export class Modal {
   contactTypes: DropdownOption[] = CONTACT_TYPES;
 
   private modalService = inject(NgbModal);
+  private fb = inject(FormBuilder);
 
   @Input() modalTitle: string = '';
   @Input() nameLabel: string = '';
@@ -35,24 +36,29 @@ export class Modal {
   @Input() addressLabel: string = '';
   @Input() legalRepresentativeLabel: string = '';
 
-  @Input() showDropdown: boolean = true;
   @Input() saveButtonLabel: string = '';
   @Input() cancelButtonLabel: string = '';
-
-  @Input() nameValue: string = '';
-  @Input() phoneValue: string = '';
-  @Input() commentValue: string = '';
-  @Input() documentNumberValue: string = '';
-  @Input() emailValue: string = '';
-  @Input() organizationNameValue: string = '';
-  @Input() addressValue: string = '';
-  @Input() legalRepresentativeValue: string = '';
 
   @Output() save = new EventEmitter<CreateContactDto>();
   @Output() cancel = new EventEmitter<void>();
 
-  contactTypeSelected = signal<DropdownOption | undefined>(undefined);
+  form!: FormGroup;
+
   closeResult: WritableSignal<string> = signal('');
+
+  constructor() {
+    this.form = this.fb.group({
+      contactTypeSelected: [null, Validators.required],
+      name: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      comments: [''],
+      documentNumber: [''],
+      email: ['', Validators.email],
+      organizationName: [''],
+      address: [''],
+      legalRepresentative: [''],
+    });
+  }
 
   open(content: TemplateRef<any>) {
     this.modalService.open(content, { ariaLabelledBy: 'modal-title' }).result.then(
@@ -66,17 +72,23 @@ export class Modal {
   }
 
   onSave(modal: any) {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const value = this.form.value;
     this.save.emit({
-      contactType: this.contactTypeSelected()?.value ?? this.contactTypes[0].value ,
-      name: this.nameValue,
-      phoneNumber: this.phoneValue,
-      comments: this.commentValue,
-      documentNumber: this.documentNumberValue,
-      email: this.emailValue,
-      organizationName: this.organizationNameValue,
-      publicOrganizationAddress: this.addressValue,
-      legalRepresentativeName: this.legalRepresentativeValue,
-      privateOrganizationAddress: this.addressValue,
+      contactType: value.contactTypeSelected.value,
+      name: value.name,
+      phoneNumber: value.phoneNumber,
+      comments: value.comments,
+      documentNumber: value.documentNumber,
+      email: value.email,
+      organizationName: value.organizationName,
+      publicOrganizationAddress: value.address,
+      legalRepresentativeName: value.legalRepresentative,
+      privateOrganizationAddress: value.address,
     });
     modal.close('Save click');
   }
@@ -87,7 +99,7 @@ export class Modal {
   }
 
   onSelectionChange(option: DropdownOption) {
-    this.contactTypeSelected.set(option);
+    this.form.get('contactTypeSelected')?.setValue(option);
   }
 
   private getDismissReason(reason: any): string {
